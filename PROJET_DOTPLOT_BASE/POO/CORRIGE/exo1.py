e7d8f8e0b808c457fd0315763c51efd52173cf6b
Sharane K.Murali <74711674+RaneMura@users.noreply.github.com>
2023-08-28 17:27:32 +0200

    Delete PROJET_DOTPLOT_BASE directory

--- PROJET_DOTPLOT_BASE/POO/CORRIGE/exo1.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:utf-8 -*-
-
-from Lib1 import *
-
-cone = Cone(1,1)
-print ("On considère un cône de rayon et hauteur unitaires")
-print ("Volume du cône (U^3) = ",round(cone.volume_cone(),2))
-print ("Surface de la base (U^2) = ",round(cone.surface(),2))
-
